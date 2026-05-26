f645a1e99b44f84b6d2c28071bc00cbc5efa8665
Douglas Lira <douglas.lira@jogoglobal.com>
2026-05-26 21:26:47 +0100

    Add background blur

--- ep1a/Source/PageBackground.hx
+++ ep1a/Source/PageBackground.hx
@@ -2,11 +2,9 @@ package;
 
 import go.BaseSprite;
 import go.Image;
-import openfl.filters.BlurFilter;
 
 class PageBackground extends BaseSprite {
 	static inline var PATH = "assets/images/background.jpg";
-	static inline var BLUR = 12.0;
 
 	var image:Image;
 
@@ -16,7 +14,6 @@ class PageBackground extends BaseSprite {
 		mouseChildren = false;
 
 		image = new Image(PATH);
-		image.filters = [new BlurFilter(BLUR, BLUR, 3)];
 		addChild(image);
 	}
 }

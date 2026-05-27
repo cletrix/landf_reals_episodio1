3ac826f605e8a40b90467d1b54cd942daf30ddf6
Henrique <henriqueva@gmail.com>
2026-05-27 16:33:02 -0300

    Responsividade

--- ep1a/Source/Game.hx
+++ ep1a/Source/Game.hx
@@ -6,15 +6,20 @@ import libs.TiledBackground;
 class Game extends BaseSprite {
 	var bg:Background;
 	var logo:Logo;
-	var tiledBg:TiledBackground;
+	var tiledBgPortrait:TiledBackground;
+	var tiledBgLandscape:TiledBackground;
 
 	#if debug
 	var debug:Debug;
 	#end
 
 	// mapScore.txt: 22 colunas × 24 linhas — mostrar o mapa inteiro
-	static inline var MAP_COLS:Int = 22;
-	static inline var MAP_ROWS:Int = 24;
+	// Portrait usar o MAP_COLS_PORTRAIT e MAP_ROWS_PORTRAIT
+	static inline var MAP_COLS_PORTRAIT:Int = 20;
+	static inline var MAP_ROWS_PORTRAIT:Int = 24;
+	// Landscape usar o MAP_COLS_LANDSCAPE e MAP_ROWS_LANDSCAPE
+	static inline var MAP_COLS_LANDSCAPE:Int = 24;
+	static inline var MAP_ROWS_LANDSCAPE:Int = 20;
 
 	public function new() {
 		super();
@@ -23,13 +28,13 @@ class Game extends BaseSprite {
 		addChild(bg);
 
 		// Fundo tileado com o mapa e tiles do futbingo
-		tiledBg = new TiledBackground(
-			"assets/maps/mapScore.txt",
-			"assets/images/tiles/box",
-			MAP_COLS,
-			MAP_ROWS
-		);
-		addChild(tiledBg);
+		tiledBgPortrait = new TiledBackground("assets/maps/mapScoreP.txt", "assets/images/tiles/box", MAP_COLS_PORTRAIT, MAP_ROWS_PORTRAIT);
+		tiledBgPortrait.visible = false;
+		addChild(tiledBgPortrait);
+
+		tiledBgLandscape = new TiledBackground("assets/maps/mapScoreL.txt", "assets/images/tiles/box", MAP_COLS_LANDSCAPE, MAP_ROWS_LANDSCAPE);
+		tiledBgLandscape.visible = false;
+		addChild(tiledBgLandscape);
 
 		logo = new Logo();
 		addChild(logo);
@@ -43,12 +48,23 @@ class Game extends BaseSprite {
 
 	public function resize(w:Float, h:Float):Void {
 		bg.resize(w, h);
+		var isLandscape = Control.isLandscape;
+		var tiledBgPortrait = tiledBgPortrait;
+		var tiledBgLandscape = tiledBgLandscape;
 
 		// Centraliza o TiledBackground na tela
-		var mapPixelW = MAP_COLS * tiledBg.tileW;
-		var mapPixelH = MAP_ROWS * tiledBg.tileH;
-		tiledBg.x = (w - mapPixelW) / 2;
-		tiledBg.y = (h - mapPixelH) / 2;
+		var mapPixelPortraitW = MAP_COLS_PORTRAIT * tiledBgPortrait.tileW;
+		var mapPixelPortraitH = MAP_ROWS_PORTRAIT * tiledBgPortrait.tileH;
+		var mapPixelLandscapeW = MAP_COLS_LANDSCAPE * tiledBgLandscape.tileW;
+		var mapPixelLandscapeH = MAP_ROWS_LANDSCAPE * tiledBgLandscape.tileH;
+
+		tiledBgPortrait.x = (w - mapPixelPortraitW) / 2;
+		tiledBgPortrait.y = (h - mapPixelPortraitH) / 2;
+		tiledBgLandscape.x = (w - mapPixelLandscapeW) / 2;
+		tiledBgLandscape.y = (h - mapPixelLandscapeH) / 2;
+
+		tiledBgPortrait.visible = !isLandscape;
+		tiledBgLandscape.visible = isLandscape;
 
 		logo.position_in_center(w / 2, h / 2);
 
